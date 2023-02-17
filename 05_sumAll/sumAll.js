c4d4b72caed0ae58abd03dcdbcf6e9bb9f8f920c
Thomas <thomas.demoncy@gmail.com>
2023-02-17 11:04:25 +0100

    should handle all the test conditions

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,24 @@
-const sumAll = function() {
+const sumAll = function(int1, int2) {
+    var lowEnd = 0;
+    var highEnd = 0;
+    let sum = 0;
 
+    if (typeof int1 !== 'number' || typeof int2 !== 'number') {
+        return 'ERROR';
+    } else if (int1 < 0 || int2 < 0) {
+        return 'ERROR';
+    } else if (int1 < int2) {
+        lowEnd = int1;
+        highEnd = int2;
+    } else {
+        lowEnd = int2;
+        highEnd = int1;
+    }
+
+    for (let i = Math.min(lowEnd, highEnd); i <= Math.max(lowEnd, highEnd); i++) {
+        sum += i;
+    }
+    return sum;
 };
 
 // Do not edit below this line
