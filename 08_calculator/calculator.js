9a332732e218e2347edaef03cd76c1afcda08b9c
Thomas <thomas.demoncy@gmail.com>
2023-02-20 15:43:34 +0100

    calculator done with all the testS.

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,33 +1,36 @@
-const add = function() {
-	
+const add = function(n1, n2) {
+    return n1 + n2;
 };
 
-const subtract = function() {
-	
+const subtract = function(n1, n2) {
+    return n1 - n2;
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+    return arr.reduce((a, b) => a + b, 0)
 };
 
-const multiply = function() {
-
+const multiply = function(arr) {
+    return arr.reduce((a, b) => a * b, 1);
 };
 
-const power = function() {
-	
+const power = function(n1, n2) {
+    return Math.pow(n1, n2);
 };
 
-const factorial = function() {
-	
+const factorial = function(number) {
+    var rval = 1;
+    for (var i = 2; i <= number; i++)
+        rval = rval * i;
+    return rval;
 };
 
 // Do not edit below this line
 module.exports = {
-  add,
-  subtract,
-  sum,
-  multiply,
-  power,
-  factorial
+    add,
+    subtract,
+    sum,
+    multiply,
+    power,
+    factorial
 };
