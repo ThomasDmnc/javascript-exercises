27fe82af90007ef23d66998bf78a74ee83fe39d0
Thomas <thomas.demoncy@gmail.com>
2023-02-23 11:14:23 +0100

    last exo done

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,23 @@
-const findTheOldest = function() {
+const findTheOldest = function(array) {
+    let newArray = [];
+    let survivor = [];
+    array.map((people) => {
+        newArray.push(people.yearOfBirth - people.yearOfDeath)
+        if (people.yearOfDeath === undefined) {
+            survivor.push(array.indexOf(people))
+        };
+    });
+    let min = Math.min(...newArray);
+    let index = newArray.indexOf(min);
+
+
+    if (survivor.length > 0) {
+        return array[survivor[0]];
+    } else {
+        return array[index];
+    };
 
 };
 
 // Do not edit below this line
-module.exports = findTheOldest;
+module.exports = findTheOldest
--- 13_caesar/caesar.js
+++ 13_caesar/caesar.js
@@ -1,5 +1,20 @@
-const caesar = function() {
+const caesar = function(string, shift) {
+    let result = [];
 
+    for (var i = 0; i < string.length; i++) {
+        let c = string[i];
+
+        if (c.match(/[a-z]/i)) {
+            var code = string.charCodeAt(i);
+            if ((code >= 65) && (code <= 90)) {
+                c = String.fromCharCode(((code - 65 + shift) % 26) + 65);
+            } else if ((code >= 97) && (code <= 122)) {
+                c = String.fromCharCode(((code - 97 + shift) % 26) + 97);
+            };
+            result.push(c);
+        };
+    };
+    return result.join('');
 };
 
 // Do not edit below this line
